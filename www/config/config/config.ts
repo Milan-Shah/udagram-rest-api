73f37766547dbabd9a56de71faa544155342a5c9
Milan Shah <mshah1992@live.com>
2020-02-28 08:30:32 -0500

    Deployed to aws:elasticbeanstalk

---
+++ www/config/config/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+  "dev": {
+    "username": process.env.UDAGRAM_USERNAME,
+    "password": process.env.UDAGRAM_PASSWORD,
+    "database": process.env.UDAGRAM_DATABASE,
+    "host": process.env.UDAGRAM_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.UDAGRAM_AWS_REGION,
+    "aws_profile": process.env.UDAGRAM_AWS_PROFILE,
+    "aws_media_bucket": process.env.UDAGRAM_AWS_MEDIA_BUCKET
+  }
+}
+
+process.on('unhandledRejection', error => {
+  // Will print "unhandledRejection err is not defined"
+  console.log('unhandledRejection', error);
+});
